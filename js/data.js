c8e788a37c26c4c64ddd1aab675c56c1d2d36f95
Guillermo García <guillermieres97@gmail.com>
2022-08-13 08:00:34 +0200

    add questions data json format

---
+++ js/data.js
@@ -0,0 +1,28 @@
+"use strict"
+
+export const quizData = [
+    {
+        question: "Who is the President of the United States?",
+        a: "Donald Trump",
+        b: "Joe Biden",
+        c: "Cristiano Ronaldo",
+        d: "Pedro Sánchez",
+        correct: "b"
+    },
+    {
+        question: "Which country has the largest population in the world?",
+        a: "China",
+        b: "India",
+        c: "EEUU",
+        d: "Portugal",
+        correct: "a"
+    },
+    {
+        question: "Which is the soccer team in Spain with the most followers?",
+        a: "Barcelona",
+        b: "Valencia",
+        c: "Real Madrid",
+        d: "Sevilla",
+        correct: "c"
+    }
+]
